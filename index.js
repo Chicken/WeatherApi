0abf2c70bf8eb7cab8bc127471ce1c849b42bb38
Antti <antti@antti.codes>
2020-06-18 12:38:30 +0300

    reworked the endpoints and made it cleaner

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const SerialPort = require('serialport');
 const SHT31 = require('raspi-node-sht31')
 const sht31 = new SHT31()
 const BMP180 = require('bmp180-sensor');
+const i2c = require('i2c-bus')
 const Readline = require('@serialport/parser-readline');
 const port = new SerialPort('/dev/serial0', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
@@ -26,7 +27,7 @@ const app = express();
 //const bodyParser = require('body-parser');
 //app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(bodyParser.json());
-const i2c = require('i2c-bus')
+
 
 let values = [],
     raw = [],
@@ -36,7 +37,7 @@ let values = [],
     latestPressure = {},
     latestLight = {};
 
-const LOGGING_LEVEL = 1; //0=nothing, 1=important, 2=website, 3=all the useless shit
+const LOGGING_LEVEL = 2; //0=nothing, 1=important, 2=website, 3=all the useless shit
 
 //dailyaverage timer
 let currentTime = new Date()
@@ -59,89 +60,116 @@ let timeout = setTimeout(()=>{
   },3*60*60*1000)
 }, timeTillStart * 60 * 1000)
 
-
-
+//main page
 app.get('/',(req, res)=>{
-if(LOGGING_LEVEL>0) console.log(formatDate(new Date())+'WEBPAGE: visited');
-res.sendFile(__dirname + '/index.html')
+  if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'WEBPAGE: visited');
+  res.sendFile(__dirname + '/index.html')
 })
 
+//public media files
 app.get('/media/:file', (req,res)=>{
-if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'WEBPAGE: media loaded');
-res.sendFile(__dirname + '/media/' + req.params.file)
+  if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'WEBPAGE: media file ' + req.params.file + ' loaded');
+  res.sendFile(__dirname + '/media/' + req.params.file)
 })
 
-/*
-app.get('/api/temp', (req,res)=>{
-if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'API: temperature and humidity data asked');
-res.send(latestTemp)
-})
-
-app.get('/api/dailyaverage', (req,res)=>{
-  if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'API: daily average temp data asked');
-  res.send({avg: yesterdayAverage})
-})
-
-app.get('/api/pressure', (req,res)=>{
-if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'API: pressure and temperature data asked');
-res.send(latestPressure)
-})
-
-app.get('/api/light', (req,res)=>{
-if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'API: light data asked');
-res.send(latestLight)
-})
-*/
-
+//api doc
 app.get('/api', (req,res)=>{
-  if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'API: api mainpage visited');
-  res.send('Incompleted<br><br>API ENDPOINTS:<br><br>  GET /api/raw - sensor raw data from 10mins<br>  GET /api/database?size=SIZE - returns the latest SIZE entries in database (all values))<br>  GET /api/parsed - sensor parsed data from 10mins<br>  GET /api/pressure - pressure and temperature<br><br>  POST /api/speed {speed: NUMBER} - wind speed to descriptive text<br>  POST /data/direction {direction: NUMBER} - wind direction to cardinal directions')
+  if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'WEBPAGE: api docs visited');
+  res.send(
+    `API DOCS<br>
+    <br>
+    all endpoints return json data.
+    GET /api/raw - raw wind data <br>
+    GET /api/parsed - parsed wind data <br>
+    GET /api/weather - all the weather info <br>
+    GET /api/gen/compass - image of wind dir compass, requires authentication<br>
+    <br>
+    &copy; Antti.Codes 2020`
+  )
 })
 
+//all the weather data
+app.get('/api/weather', (req,res)=>{
+  if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'API: weather data asked');
+  res.send({
+    windSpeedNow: values[values.length-1].speed,
+    windSpeedNowText: getSpeedText(values[values.length-1].speed),
+    windDirNow: values[values.length-1].direction,
+    windDirNowText: getDirectionText(values[values.length-1].direction),
+    windGust: Math.max(...values.map(v=>v.speed)),
+    windGustText: getSpeedText(Math.max(...values.map(v=>v.speed))),
+    windSpeedAvg: parseFloat(arrAvg(values.map(v=>v.speed)).toFixed(1)),
+    windSpeedAvgText: getSpeedText(arrAvg(values.map(v=>v.speed))),
+    windDirAvg: Math.round(getAverage(values.map(v=>v.direction)).toFixed(1)),
+    windDirAvgText: getDirectionText(getAverage(values.map(v=>v.direction))),
+		temperature: latestTemp.temp,
+		dailyTempAvg: yesterdayAverage,
+		humidity: latestTemp.hum,
+		pressure: latestPressure.pressure,
+		lightness: latestLight.light,
+		dewPoint: parseFloat(dewPoint(latestTemp.temp, latestTemp.hum).toFixed(1)),
+		absoluteHumidity: parseFloat(absoluteHumidity(latestTemp.temp, latestTemp.hum).toFixed(1)),
+		feelsLikeTemp: feelsLikeTemp(latestTemp.temp, values[values.length-1].speed)
+  })
+})
+
+//raw wind sensor data
 app.get('/api/raw', (req,res)=>{
-  if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'API: raw data asked');
+  if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'API: raw data asked');
   res.send(raw)
 })
 
+//parsed wind sensor data
 app.get('/api/parsed', (req,res)=>{
-if(LOGGING_LEVEL>1) console.log(formatDate(new Date())+'API: parsed data asked');
-res.send(values)
+  if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'API: parsed data asked');
+  res.send(values)
 })
 
 app.listen(8080, ()=>{
-if(LOGGING_LEVEL>0) console.log(formatDate(new Date())+'APP: server online')
+  if(LOGGING_LEVEL>0) console.log(formatDate(new Date())+'APP: server online')
 })
 
 async function readTemp() {
   try{
-  let data = await sht31.readSensorData();
-  let temp = parseFloat(data.temperature.toFixed(1));
-  let hum = Math.round(data.humidity);
-  if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'SENSOR: new temperature and humidity data ' + hum + " " + temp)
-  return {temp: temp, hum: hum};
-  } catch(e) {console.log(formatDate(new Date())+'ERROR: sht31,',e)}
+    let data = await sht31.readSensorData();
+    let temp = parseFloat(data.temperature.toFixed(1));
+    let hum = Math.round(data.humidity);
+    if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'SENSOR: new temperature and humidity data ' + hum + " " + temp)
+    return {
+      temp: temp,
+      hum: hum
+    };
+  } catch(e) {
+    console.log(formatDate(new Date())+'ERROR: sht31,',e)
+  }
 };
 
 async function readPressure() {
   try{
-  let bmp180 = await BMP180({address:0x77,mode:3,units:'metric'})
-  let data = await bmp180.read()
-  if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'SENSOR: new pressure and temperature data ' + data.pressure)
-  await bmp180.close();
-  data.pressure = Math.round(data.pressure/100);
-  return data
-  } catch(e) {console.log(formatDate(new Date())+'ERROR: bmp180,',e)}
+    let bmp180 = await BMP180({address:0x77,mode:3,units:'metric'})
+    let data = await bmp180.read()
+    if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'SENSOR: new pressure and temperature data ' + data.pressure)
+    await bmp180.close();
+    data.pressure = Math.round(data.pressure/100);
+    return data
+  } catch(e) {
+    console.log(formatDate(new Date())+'ERROR: bmp180,',e)
+  }
 };
 
 async function readLight() {
-try{
-let buffer = await new Buffer.alloc(2);
-let i2c1 = await i2c.openPromisified(1);
-await i2c1.readI2cBlock(0x5c, 0x20, 2, buffer);
-if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'SENSOR: new light data ' + ((buffer[1] + (256 * buffer[0])) / 1.2))
-await i2c1.close();
-return {light: parseFloat(((buffer[1] + (256 * buffer[0])) / 1.2).toFixed(1))}
-} catch(e) {console.log(formatDate(new Date())+'ERROR: bh1750,',e)}
+  try{
+    let buffer = await new Buffer.alloc(2);
+    let i2c1 = await i2c.openPromisified(1);
+    await i2c1.readI2cBlock(0x5c, 0x20, 2, buffer);
+    if(LOGGING_LEVEL>2) console.log(formatDate(new Date())+'SENSOR: new light data ' + ((buffer[1] + (256 * buffer[0])) / 1.2))
+    await i2c1.close();
+    return {
+      light: parseFloat(((buffer[1] + (256 * buffer[0])) / 1.2).toFixed(1))
+    }
+  } catch(e) {
+    console.log(formatDate(new Date())+'ERROR: bh1750,',e)
+  }
 }
 
 parser.on('data', async data =>{
@@ -151,8 +179,8 @@ parser.on('data', async data =>{
   data=data.split(',')
   if(LOGGING_LEVEL>0&&data[5].startsWith('V')) console.log(formatDate(new Date())+'SENSOR: invalid data', data);
   data={
-  direction: parseInt(data[1]),
-  speed: parseFloat(data[3])
+    direction: parseInt(data[1]),
+    speed: parseFloat(data[3])
   }
   if(values.length>=600) values.shift();
   values.push(data);
@@ -161,79 +189,80 @@ parser.on('data', async data =>{
   latestLight = await readLight();
 });
 
-function getDirection(dir) {
-if(dir>=337.5||dir<22.5){
-	return "Pohjoinen"
-}
-if(dir>=22.5&&dir<67.5){
-	return "Koillinen"
-}
-if(dir>=67.5&&dir<112.5){
-	return "Itä"
-}
-if(dir>=112.5&&dir<157.5){
-	return "Kaakko"
-}
-if(dir>=157.5&&dir<202.5){
-	return "Etelä"
-}
-if(dir>=202.5&&dir<247.5){
-	return "Lounas"
-}
-if(dir>=247.5&&dir<292.5){
-	return "Länsi"
-}
-if(dir>=292.5&&dir<337.5){
-	return "Luode"
-}
-return 'ERROR';
+function getDirectionText(dir) {
+  if(dir>=337.5||dir<22.5){
+  	return "Pohjoinen"
+  }
+  if(dir>=22.5&&dir<67.5){
+  	return "Koillinen"
+  }
+  if(dir>=67.5&&dir<112.5){
+  	return "Itä"
+  }
+  if(dir>=112.5&&dir<157.5){
+  	return "Kaakko"
+  }
+  if(dir>=157.5&&dir<202.5){
+  	return "Etelä"
+  }
+  if(dir>=202.5&&dir<247.5){
+  	return "Lounas"
+  }
+  if(dir>=247.5&&dir<292.5){
+  	return "Länsi"
+  }
+  if(dir>=292.5&&dir<337.5){
+  	return "Luode"
+  }
+  return 'ERROR';
 }
 
-function getSpeed(speed) {
-  speed = parseFloat(speed);
+function getSpeedText(speed) {
   if(speed<1){
     return "Tyyntä"
   } else if(speed>=1&&speed<4){
-	return "Heikkoa tuulta"
+	  return "Heikkoa tuulta"
   } else if(speed>=4&&speed<8){
-	return "Kohtalaista tuulta"
+	  return "Kohtalaista tuulta"
   } else if(speed>=8&&speed<14){
-	return "Navakkaa tuulta"
+  	return "Navakkaa tuulta"
   } else if(speed>=14&&speed<21){
-	return "Kovaa tuulta"
+	  return "Kovaa tuulta"
   } else if(speed>=21&&speed<33){
-	return "Myrskyä"
+	  return "Myrskyä"
   } else if(speed>=33){
-	return "Hirmumyrskyä"
+	  return "Hirmumyrskyä"
   } else {
-	return 'ERROR'
+	  return 'ERROR'
   }
 }
 
-const arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
-
-function deg2rad(degrees) {
-	return degrees * (Math.PI/180);
+function arrAvg(arr) {
+  return arr.reduce((a,b) => a + b, 0) / arr.length;
 }
 
-function rad2deg(radians){
-	return radians * (180/Math.PI);
+function degToRad(degrees) {
+  return degrees * (Math.PI/180);
 }
 
-function get_average(angles) {
-	let sin_sum = 0;
-	let cos_sum = 0;
+function radToDeg(radians){
+  return radians * (180/Math.PI);
+}
+
+function getAverage(angles) {
+	let sinSum = 0;
+	let cosSum = 0;
 	
 	for(angle of angles){
-		let r = deg2rad(angle)
-		sin_sum += Math.sin(r)
-		cos_sum += Math.cos(r)
+		let r = degToRad(angle)
+		sinSum += Math.sin(r)
+		cosSum += Math.cos(r)
 	}
 	let flen = angles.length
-	let s = sin_sum / flen;
-        let c = cos_sum / flen;
+	let s = sinSum / flen;
+        let c = cosSum / flen;
 
-	let arc = rad2deg(Math.atan(s/c))
+	let arc = radToDeg(Math.atan(s/c))
 	let average = 0;
 	if(s>0&&c>0) {
 		average = arc;
@@ -246,22 +275,22 @@ function get_average(angles) {
 	return average;
 }
 
-function dew_point(temp,hum) {
+function dewPoint(temp,hum) {
 	let H = (Math.log10(hum)-2)/0.4343+(17.62*temp)/(243.12+temp);
 	return 243.12*H/(17.62-H);
 }
 
-function absolute_humidity(temp,hum) {
+function absoluteHumidity(temp,hum) {
 	return 216.7*(hum/100*6.112*Math.exp(17.62*temp/(243.12+temp))/(273.15+temp));
 }
 
-function feels_like(T,V) {
+function feelsLikeTemp(T,V) {
 	if(T>10||V<1.5) return T;
 	return 13.12+0.6215*T-13.956*Math.pow(V,0.16)+0.4867*T*Math.pow(V,0.16)
 }
 
 function formatDate(date) {
-return '[' + (date.getDate()) + '.' + (date.getMonth()+1) + '.' + date.getFullYear() + ' | ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '] ';
+  return `[${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} | ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}] `;
 }
 
 if(LOGGING_LEVEL>0) console.log(formatDate(new Date())+'APP: started')
